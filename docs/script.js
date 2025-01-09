22641939066ec93646f240578b4d383f46a2a7a8
Carson Lam <lamcn51@gmail.com>
2025-01-08 23:58:26 -0500

    added multiple buttons

--- docs/script.js
+++ docs/script.js
@@ -34,7 +34,44 @@ document.getElementById('problemForm').addEventListener('submit', async function
                 model: 'llama-3.3-70b-versatile'
             })
         });
-        console.log('HIIIIII');
+
+        const completion = await response.json();
+
+        conversationHistory.push({
+            role: 'assistant',
+            content: completion.choices[0].message.content
+        })
+
+
+        document.getElementById('response').innerHTML = `<p><strong>Groq AI Response:</strong> ${completion.choices[0].message.content}</p>`;
+    } catch (error) {
+        console.error('Error: ', error.message);
+        document.getElementById('response').innerHTML = 'Error fetching response'
+    }
+});
+
+ocument.getElementById('meanButton').addEventListener('submit', async function (event) {
+    
+    event.preventDefault();
+    const query = document.getElementById('problem').value;
+    document.getElementById('response').innerHTML = "Fetching response from Groq AI...";
+
+    conversationHistory.push({
+        role: 'user',
+        content: query
+    })
+
+    try {       
+        const response = await fetch(`https://studybyte.onrender.com/study`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                messages: conversationHistory,
+                model: 'llama-3.3-70b-versatile'
+            })
+        });
 
         const completion = await response.json();
 
